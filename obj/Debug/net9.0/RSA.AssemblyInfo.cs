7e7fa3903c94f2f9805dfb95de6eb643b566db5b
MirrorPro <644328850@qq.com>
2025-03-06 22:36:16 +0800

    清理dotnet7的文件夹

--- obj/Debug/net9.0/RSA.AssemblyInfo.cs
+++ obj/Debug/net9.0/RSA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc36d33555bfab52d5cb1b5f001dbfd1cca149d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b135c4f195cfa99cff2871f0890f468d4bcd7b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
